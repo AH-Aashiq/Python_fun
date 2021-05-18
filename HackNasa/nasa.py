b9233f9d30220f4cebdf75cc3393db578ab778a6
Ariful haque Asiek <66855223+AH-Aashiq@users.noreply.github.com>
2021-05-18 20:04:20 +0600

    Add files via upload

---
+++ HackNasa/nasa.py
@@ -0,0 +1,7 @@
+import time
+for i in range(0, 100, 2):
+    print("Nasa hacking", i, "%")
+    time.sleep(.5)
+print("NASA hacked successfully....")
+
+input()
---
+++ autoText/autoText.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+while True:
+     time.sleep(4)
+     pyautogui.typewrite('I love so much')
+     time.sleep(4)
+     pyautogui.press('enter')
